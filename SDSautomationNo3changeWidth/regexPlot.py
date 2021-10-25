d587845d23d54b5aa77e642800be3ecb77c30b61
Ye <k.ye@utwente.nl>
2021-10-25 15:14:32 +0200

    revise regexPlot.py

--- SDSautomationNo3changeWidth/regexPlot.py
+++ SDSautomationNo3changeWidth/regexPlot.py
@@ -14,16 +14,18 @@ plt.style.use(['science','no-latex'])
 
 fileList = glob.glob('results\*\*.csv')
 fig, subplots = plt.subplots(1, 1, dpi=200, figsize=(6, 4))
-data = []
+data = pd.DataFrame()
+SAVEFILE = 0
 for file in fileList:
     label = re.search(r'\d+nm', file).group(0)
     dataSingle = pd.read_csv(file, header=None)
     dataSingle.columns = ['freq'+str(label), 'SBSgain'+str(label)]
-    data.append(dataSingle)
+    data = pd.concat([data, dataSingle], axis=1)
     subplots.plot(dataSingle.iloc[:,0], dataSingle.iloc[:,1], label=str(label))
     subplots.set_xlabel('Acoustic frequency (GHz)')
     subplots.set_ylabel('Brillouin gain (m-1W-1)')
     subplots.set_title('Brillouin gain of 180/450/180 nm SDS at ' + str(label))
     directory = re.search(r'^\w+\\\w+\(\d+\w+\)\\', file).group(0)
-    plt.savefig(directory+'SBSgain'+str(label)+'.png')
+    if SAVEFILE:
+        plt.savefig(directory+'SBSgain'+str(label)+'.png')
     plt.cla()
