b112145c0d1cbb463f39a1be2ac52835dafc49fd
Ye <k.ye@utwente.nl>
2021-10-28 12:11:32 +0200

    Add the feature to store all samples

--- README.md
+++ README.md
@@ -7,4 +7,9 @@ This project aims at optimizing the SDS geometry to find the highest SBS gain.
 - [X] Create the initial population
 - [X] Create the second generation without mutation
 - [x] Mutate the second generation
+- [ ] Add fprintf to indicate the status of the program
 - [ ] Increase the size of the population
+
+
+## Issues
+* diffProp could become zero after several rounds
--- geneticAlgorithm/generations.m
+++ geneticAlgorithm/generations.m
@@ -7,74 +7,104 @@ close all;
 format long;
 
 % Genetic pool for the first generation
-tg_span = [160, 170, 180, 190];
-tint_span = [450, 470, 490, 510];
-tc_span = [7.8, 7.9, 8.0, 8.1];
-w_span = [2800, 2900, 3000, 3100];
+tg_span = linspace(160, 190, 31);
+tint_span = linspace(450, 510, 13);
+tc_span = linspace(7.8, 8.3, 51);
+w_span = linspace(2800, 3100, 31);
 
 population = 20; % Population for the genetic evolution
-samples = table(zeros(population, 1), zeros(population, 1), ...
-    zeros(population, 1), zeros(population, 1), rand(population, 1), rand(population, 1)); % table that store the geometry of the samples
-samples.Properties.VariableNames = {'tg' 'tint' 'tc' 'w' 'gain' 'freq'};
+candidates = table(zeros(population, 1), zeros(population, 1), ...
+    zeros(population, 1), zeros(population, 1), rand(population, 1), rand(population, 1)); % table that store the geometry of the candidates
+candidates.Properties.VariableNames = {'tg' 'tint' 'tc' 'w' 'gain' 'freq'};
 
 % Generate the first generation
 for i = 1:population
-    samples.tg(i) = tg_span(randi(4));
-    samples.tint(i) = tint_span(randi(4));
-    samples.tc(i) = tc_span(randi(4));
-    samples.w(i) = w_span(randi(4));
+    candidates.tg(i) = tg_span(randi(length(tg_span)));
+    candidates.tint(i) = tint_span(randi(length(tint_span)));
+    candidates.tc(i) = tc_span(randi(length(tc_span)));
+    candidates.w(i) = w_span(randi(length(w_span)));
 end
 
-% Check and remove the redundant
-samples = unique(samples, 'rows', 'first');
+samples = table(zeros(130, 1), zeros(130, 1), ...
+    zeros(130, 1), zeros(130, 1), zeros(130, 1), zeros(130, 1));
 
-candidates = samples; % Candidates of the first generation
+samples(linspace(1, 20, 20), :) = candidates; % Candidates of the first generation
+samples.Properties.VariableNames = {'tg' 'tint' 'tc' 'w' 'gain' 'freq'};
 
-% % Calculate the SBS gain of each sample in the first generation
-% for i = 1:population
-%     output = runCOMSOL(candidates(i));
-%     maxgain.gain(i) = output.gain;
-%     maxgain.freq(i) = output.freq;
-% end
+rounds = 10; % 10 rounds for evolutions
 
-% Select half of the candidates with the highest gain
-[~, ind] = maxk(candidates.gain, height(candidates) / 2);
-elites = candidates(ind, :);
-kids = zeros(height(elites), width(elites));
-kids = array2table(kids);
-kids.Properties.VariableNames = {'tg' 'tint' 'tc' 'w' 'gain' 'freq'};
+for round = 1:rounds
+    newCandis = find(~candidates.gain); % Find the new candidates whose gain haven't been calculated
 
-% Generate the other half of the candidates from the elites
-for i = 1:height(elites) / 2
-    j = randperm(height(elites), 2); % Randomly select two elites
-    kidsA = elites(j(1), :);
-    kidsB = elites(j(2), :);
-    kidsA(1, [5, 6]) = table(0, 0); % Initialize the gain and corresponding frequencies of kidsA
-    kidsB(1, [5, 6]) = table(0, 0); % Initialize the gain and corresponding frequencies of kidsB
-    diffProp = find(kidsA{:, :} - kidsB{:, :}); % Get the different parameters between A and B
-    propInd = diffProp(randi(length(diffProp))); % Randomly determine which property to swap
-    propValue = kidsA(1, propInd);
-    kidsA(1, propInd) = kidsB(1, propInd);
-    kidsB(1, propInd) = propValue;
-    kids(2 * i - 1, :) = kidsA;
-    kids(2 * i, :) = kidsB;
-end
+    if newCandis
 
-% mutatate the kids
-for i = 1:height(kids)
-    j = randi(100);
+        % Calculate the SBS gain of the candidates
+        for i = 1:length(newCandis)
+            geom = table2struct(candidates(newCandis(i), :)); % newCandis(i) is the index of the newCandis candidate
+            % SBS = runCOMSOL(geom);
+            % candidates.gain(newCandis(i)) = SBS.gain;
+            % candidates.freq(newCandis(i)) = SBS.freq;
 
-    if (j > 50) && (j < 65) % mutation probability is 14 %
-        k = randi(4); % randomly select one of the four freedoms to mutate
+            % code for trial run
+            candidates.gain(newCandis(i)) = rand(1);
+            candidates.freq(newCandis(i)) = 0;
+        end
 
-        if mod(j, 2) == 1
-            kids(i, k) = table(kids{i, k} * 1.1);
+        firstZero = find(~samples.tg); % find the pointer to store infomation of new samples in the 'samples' array
+        samples(linspace(firstZero(1), firstZero(1) + length(newCandis) - 1, length(newCandis)), :) = candidates(newCandis, :);
+    end
+
+    % Select half of the candidates with the highest gain
+    [~, index] = maxk(candidates.gain, height(candidates) / 2);
+    elites = candidates(index, :);
+    kids = zeros(height(elites), width(elites));
+    kids = array2table(kids);
+    kids.Properties.VariableNames = {'tg' 'tint' 'tc' 'w' 'gain' 'freq'};
+
+    % Generate the other half of the candidates from the elites
+    for i1 = 1:height(elites) / 2
+        j1 = randperm(height(elites), 2); % Randomly select two elites
+        kidsA = elites(j1(1), :);
+        kidsB = elites(j1(2), :);
+        kidsA(1, [5, 6]) = table(0, 0); % Initialize the gain and corresponding frequencies of kidsA
+        kidsB(1, [5, 6]) = table(0, 0); % Initialize the gain and corresponding frequencies of kidsB
+        diffProp = find(kidsA{:, :} - kidsB{:, :}); % Get the different parameters between A and B
+
+        if diffProp
+            propInd = diffProp(randi(length(diffProp))); % Randomly determine which property to swap
+            propValue = kidsA(1, propInd);
+
+            if ~propValue{:, :}
+                disp(num2str(propInd));
+                error('ERROR');
+            end
+
+            kidsA(1, propInd) = kidsB(1, propInd);
+            kidsB(1, propInd) = propValue;
+            kids(2 * i1 - 1, :) = kidsA;
+            kids(2 * i1, :) = kidsB;
         else
-            kids(i, k) = table(kids{i, k} / 1.1);
+            error('same kids'); % If the two kids are the same, then repair this trial
         end
 
     end
 
-end
+    % mutatate the kids
+    for i2 = 1:height(kids)
+        j2 = randi(100);
 
-candidates = [elites; kids]; % candidates for the next generation
+        if (j2 > 50) && (j2 < 65) % mutation probability is 14 %
+            k = randi(4); % randomly select one of the four freedoms to mutate
+
+            if mod(j2, 2) == 1
+                kids(i2, k) = table(kids{i2, k} * 1.1);
+            else
+                kids(i2, k) = table(kids{i2, k} / 1.05);
+            end
+
+        end
+
+    end
+
+    candidates = [elites; kids]; % candidates for the next generation
+end
